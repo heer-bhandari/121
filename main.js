82cee5ab78f38ae820ff4b307481805a3fd81ab5
heer-bhandari <75120883+heer-bhandari@users.noreply.github.com>
2022-06-10 17:56:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function setup(){
+    canvas=createCanvas(200,200);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+function clearCanvas(){
+background("white");
+}
+
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function draw(){  
+// Set stroke wieght to 13
+strokeWeight(13);
+// Set stroke color to black
+stroke(0);
+// If mouse is pressed , draw line between previous and current mouse positions 
+if (mouseIsPressed) {
+line (pmouseX , pmouseY , mouseX , mouseY);
+}
+}
+
+function classifyCanvas() {
+classifier.classify(canvas , gotResult);
+}
+
+function gotResult(error , results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML = "Label: " + results[0].label;
+
+    document.getElementById("confidence").innerHTML = "Confidence: " + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new SpeechSynthesisUtterence(result[0].label);
+    synth.speak(utterThis);
+}
